5bea52c74d8abe7a64efbb19cef03b128aff2f79
QuantschnigAlex <alex.quantschnig@gmx.at>
2024-11-11 09:54:54 +0100

    center allowancetab

--- src/components/web3/AllowanceTab.tsx
+++ src/components/web3/AllowanceTab.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useWeb3Context } from "../../hooks/useWeb3";
 import { AllowanceInfo } from "../../types/web3";
 import { AllowanceScanner } from "../../services/AllowanceScanner";
-import { Spin } from "antd";
+import { Space, Spin } from "antd";
 import { AllowanceTile } from "./AllowanceTile";
 
 export const AllowanceList: React.FC = () => {
@@ -16,7 +16,7 @@ export const AllowanceList: React.FC = () => {
         setLoading(true);
         const scanner = new AllowanceScanner(provider);
         let options = {
-          blockRange: 60000,
+          blockRange: 1000000,
         };
         const allowanceList = await scanner.scanWalletAllowances(
           account,
@@ -32,13 +32,23 @@ export const AllowanceList: React.FC = () => {
   if (!account) return null;
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
       {loading && <Spin />}
       {allowances.length === 0 && !loading && <p>No allowances found</p>}
-      {!loading &&
-        allowances.map((allowance) => (
-          <AllowanceTile key={allowance.spender} {...allowance} />
-        ))}
+      <Space direction="vertical" size="large">
+        {!loading &&
+          allowances.map((allowance) => (
+            <AllowanceTile key={allowance.spender} {...allowance} />
+          ))}
+      </Space>
     </div>
   );
 };
