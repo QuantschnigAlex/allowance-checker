0e5db161256bb2c97c12d1c372c182fcf94aa7a2
QuantschnigAlex <alex.quantschnig@gmx.at>
2024-12-26 20:58:30 +0100

    update main.tf

--- main.tf
+++ main.tf
@@ -4,21 +4,25 @@ provider "aws" {
 
 resource "aws_security_group" "allow_ssh" {
   name = "allow_ssh"
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0eddb4a4e7d846d6f"  #Amazon Linux 2023 AMI
+  ami           = "ami-0eddb4a4e7d846d6f"
   instance_type = "t2.micro"
   key_name      = "allowance_checker"
-  vpc_security_groups = [aws_security_group.allow_ssh.id]
-
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
     Name = "AppServer"
   }
